534ea822c6959a9c4855fda9e689f27c402bd86e
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-08-28 16:54:44 +0300

    add drawer menu

--- app/src/main/java/karpenko/test/kitsuapp/view/MainActivity.kt
+++ app/src/main/java/karpenko/test/kitsuapp/view/MainActivity.kt
@@ -1,32 +1,41 @@
 package karpenko.test.kitsuapp.view
 
 import android.os.Bundle
-import android.view.Gravity
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.customview.widget.ViewDragHelper
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import karpenko.test.kitsuapp.R
 import karpenko.test.kitsuapp.databinding.ActivityMainBinding
-import karpenko.test.kitsuapp.viewmodel.AnimListViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setSupportActionBar(binding.topAppBar)
+
+
         drawerLayout = binding.navDrawableMenu
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         navController = Navigation.findNavController(this, R.id.fragmentContainerView)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navigationView, navController)
@@ -36,5 +45,11 @@ class MainActivity : AppCompatActivity() {
         return NavigationUI.navigateUp(navController,drawerLayout)
 
     }
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }*/
 
 }

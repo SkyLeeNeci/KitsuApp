35e2a0aca9c953172ca654a3f7293f607352fe82
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-08-31 11:31:28 +0300

    add manga detail

---
+++ app/src/main/java/karpenko/test/kitsuapp/view/fragment/MangaDetailFragment.kt
@@ -0,0 +1,56 @@
+package karpenko.test.kitsuapp.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import karpenko.test.kitsuapp.R
+import karpenko.test.kitsuapp.databinding.FragmentMangaDetailBinding
+import karpenko.test.kitsuapp.databinding.ItemMangaBinding
+import karpenko.test.kitsuapp.viewmodel.AnimeDetailViewModel
+import karpenko.test.kitsuapp.viewmodel.MangaDetailViewModel
+import karpenko.test.kitsuapp.viewmodel.MangaListViewModel
+
+class MangaDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentMangaDetailBinding
+    private var mangaId = 0
+    private val viewModel: MangaDetailViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[MangaDetailViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        arguments?.let {
+            mangaId = MangaDetailFragmentArgs.fromBundle(it).mangaId
+        }
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_manga_detail, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.fetch(mangaId)
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.mangaDetailLiveData.observe(viewLifecycleOwner){
+            it?.let {
+                binding.attributes = it
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/karpenko/test/kitsuapp/viewmodel/MangaDetailViewModel.kt
@@ -0,0 +1,25 @@
+package karpenko.test.kitsuapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import karpenko.test.kitsuapp.model.database.AnimeDatabase
+import karpenko.test.kitsuapp.model.pojo.mangaPojo.MangaAttributes
+import kotlinx.coroutines.launch
+
+class MangaDetailViewModel(application: Application): BaseViewModel(application) {
+
+    private val _mangaDetailLiveData = MutableLiveData<MangaAttributes>()
+    val mangaDetailLiveData: LiveData<MangaAttributes>
+        get() = _mangaDetailLiveData
+
+    fun fetch(mangaId: Int){
+
+        launch {
+            val manga = AnimeDatabase.getInstance(getApplication()).mangaDao().getManga(mangaId)
+            _mangaDetailLiveData.value = manga
+        }
+
+    }
+
+}
